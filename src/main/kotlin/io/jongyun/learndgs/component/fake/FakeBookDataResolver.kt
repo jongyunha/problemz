f7e7be9f33cf9f4ce52b04610762b6e62debd665
neimen <neimen.95@kakaostyle.com>
2022-07-22 14:01:04 +0900

    DatafetchingEnvironment

--- src/main/kotlin/io/jongyun/learndgs/component/fake/FakeBookDataResolver.kt
+++ src/main/kotlin/io/jongyun/learndgs/component/fake/FakeBookDataResolver.kt
@@ -1,8 +1,12 @@
 package io.jongyun.learndgs.component.fake
 
+import com.netflix.dgs.codegen.generated.DgsConstants
 import com.netflix.dgs.codegen.generated.types.Book
+import com.netflix.dgs.codegen.generated.types.ReleaseHistory
+import com.netflix.dgs.codegen.generated.types.ReleaseHistoryInput
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
 import com.netflix.graphql.dgs.InputArgument
 import io.jongyun.learndgs.datsource.fake.FakeBookDataSource
 import org.springframework.util.StringUtils
@@ -18,4 +22,24 @@ class FakeBookDataResolver {
             FakeBookDataSource.BOOK_LIST.filter { book -> book.author.name == author }
         }
     }
+
+    @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.BooksByReleased)
+    fun getBooksByReleased(dataFetchingEnvironment: DgsDataFetchingEnvironment): List<Book> {
+        val releasedMap = dataFetchingEnvironment.getArgument<Map<String, Any>>("releasedInput")
+
+        val releaseHistoryInput = ReleaseHistoryInput(
+            printedEdition = releasedMap["printedEdition"] as Boolean,
+            year = releasedMap["year"] as Int
+        )
+
+        return FakeBookDataSource.BOOK_LIST.filter { book: Book ->
+            matchReleaseHistory(
+                releaseHistoryInput,
+                book.released
+            )
+        }
+    }
+
+    fun matchReleaseHistory(input: ReleaseHistoryInput, element: ReleaseHistory?) =
+        input.printedEdition == element?.printedEdition && input.year == element?.year
 }
