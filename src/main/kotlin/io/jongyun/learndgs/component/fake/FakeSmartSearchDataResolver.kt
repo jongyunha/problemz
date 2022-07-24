17837078a933a4073ca7400ad7f9ad37cfe94a95
neimen <popawaw@naver.com>
2022-07-24 12:56:00 +0900

    Union type

---
+++ src/main/kotlin/io/jongyun/learndgs/component/fake/FakeSmartSearchDataResolver.kt
@@ -0,0 +1,29 @@
+package io.jongyun.learndgs.component.fake
+
+import com.netflix.dgs.codegen.generated.DgsConstants
+import com.netflix.dgs.codegen.generated.types.SmartSearchResult
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
+import com.netflix.graphql.dgs.InputArgument
+import io.jongyun.learndgs.datsource.fake.FakeBookDataSource
+import io.jongyun.learndgs.datsource.fake.FakeHelloDataSource
+
+@DgsComponent
+class FakeSmartSearchDataResolver {
+
+    @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.SmartSearch)
+    fun getSmartSearch(@InputArgument(name = "keyword") keyword: String?): List<SmartSearchResult> {
+        val smarSearchList = ArrayList<SmartSearchResult>();
+        when (keyword) {
+            null -> {
+                smarSearchList.addAll(FakeBookDataSource.BOOK_LIST)
+                smarSearchList.addAll(FakeHelloDataSource.HELLO_LIST)
+            }
+            else -> {
+                FakeHelloDataSource.HELLO_LIST.filter { it.text.equals(keyword) }.forEach(smarSearchList::add)
+                FakeBookDataSource.BOOK_LIST.filter { it.title.equals(keyword) }.forEach(smarSearchList::add)
+            }
+        }
+        return smarSearchList
+    }
+}
