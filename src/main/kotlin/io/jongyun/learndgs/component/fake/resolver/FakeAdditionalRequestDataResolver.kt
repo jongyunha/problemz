9b35ed50d1936aab11b3d464bf9641a8d6787200
neimen <popawaw@naver.com>
2022-07-24 21:03:40 +0900

    Http header

---
+++ src/main/kotlin/io/jongyun/learndgs/component/fake/resolver/FakeAdditionalRequestDataResolver.kt
@@ -0,0 +1,28 @@
+package io.jongyun.learndgs.component.fake.resolver
+
+import com.netflix.dgs.codegen.generated.DgsConstants
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsData
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestParam
+
+@DgsComponent
+class FakeAdditionalRequestDataResolver {
+
+    @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.AdditionalRequest)
+    fun additionalOnRequest(
+        @RequestHeader(required = false) optionalHeader: String,
+        @RequestHeader(required = true) mandatoryHeader: String,
+        @RequestParam(required = false) optionalParam: String,
+        @RequestParam(required = true) mandatoryParam: String
+    ): String {
+        return with(StringBuilder()) {
+            append("Optional header ${optionalHeader}, ")
+            append("Mandatory header ${mandatoryHeader}, ")
+            append("Optional param ${optionalParam}, ")
+            append("Mandatory param ${mandatoryParam}")
+            toString()
+        }
+    }
+
+}
